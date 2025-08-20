a85996eb202696738cd104753cb3e57a6deab94e
kamonnan <kamonnan@kkumail.com>
2025-08-20 13:10:28 +0700

    fix: fix route and fix detail in hoomepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :home, only: [ :index, :create, :update, :destroy ] do
     member { patch :toggle }
   end
-  get :brag, to: "home#brag", as: :brag
+
+  get "/home/brag", to: "home#brag", as: :brag
+
   post "/tasks", to: "home#create", as: :tasks
 
   patch "/tasks/:id/toggle_status", to: "home#toggle_status", as: :toggle_status_task
